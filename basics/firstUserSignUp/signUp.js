301256686460c7feeba496fa6a5516612882a33b
Moonwalker <abhisk.mhra@gmail.com>
2021-03-03 22:52:40 +0530

    init

---
+++ basics/firstUserSignUp/signUp.js
@@ -0,0 +1,8 @@
+const uid = "abc1";
+
+var fullName = "Abhishek Mishra";
+var email = "abhishk.mhra@gmail.com"
+var passwd = 123456;
+var confirmPassword = 123456;
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
---
+++ basics/vars&dtypes/variable.js
@@ -0,0 +1,13 @@
+var  fullName = " Abhishek Mishra ";
+
+var isLoggedIn = true;
+
+console.log(isLoggedIn);
+
+var paymentMethod;
+
+console.log(paymentMethod);
+
+paymentMethod = "Credit Card";
+
+console.log(paymentMethod);
