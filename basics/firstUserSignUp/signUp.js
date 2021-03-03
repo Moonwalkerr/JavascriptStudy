96e813f50da19260eae5b399cbb3785551ee3110
Moonwalker <abhisk.mhra@gmail.com>
2021-03-03 23:06:29 +0530

    basic signup cli app

--- basics/firstUserSignUp/signUp.js
+++ basics/firstUserSignUp/signUp.js
@@ -6,3 +6,13 @@ var passwd = 123456;
 var confirmPassword = 123456;
 var courseCount = 0;
 var isLoggedInFromGoogle = false;
+
+
+console.log(`
+
+    The Unique Id of User is : ${uid} 
+    The User Name is         : ${fullName}
+    The Email Address is     : ${email}
+    And the Password         : ${passwd}
+
+`)
