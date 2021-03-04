a58ffea309bda43e412ca74b60b93d7da79fd689
Moonwalker <abhisk.mhra@gmail.com>
2021-03-04 22:02:11 +0530

    discount calculator

--- basics/operators/discount.js
+++ basics/operators/discount.js
@@ -6,3 +6,5 @@ var costPrice = 690;
 discount = ((costPrice - sellingPrice)/costPrice)  * 100;
 betterDiscountPercentage = Math.round(discount); // rounding things up using Math library
 console.log("The Discount Percentage is : " +betterDiscountPercentage, "% off");
+
+console.log(typeof betterDiscountPercentage);
