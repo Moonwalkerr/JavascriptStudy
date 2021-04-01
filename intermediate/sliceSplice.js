1929d1c732aac4948aee43646d15b5e8794993bd
Moonwalker <abhisk.mhra@gmail.com>
2021-04-02 00:11:45 +0530

    slice & splice operations in arrays in js

---
+++ intermediate/sliceSplice.js
@@ -0,0 +1,8 @@
+var arr1 = ['a','v','s','d','e'];
+
+// console.log(arr1.slice(1,4));
+
+var users = ['Abhi','Harry','Ram','Shyam','Mohan','Roy','Sam'];
+
+users.splice(1,4,'HI','bye','dsf','sd');
+console.log(users);
