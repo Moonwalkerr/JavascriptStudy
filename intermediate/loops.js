5524fa0ad09cee94b1e044a83c259fe7e63a4b2f
Moonwalker <abhisk.mhra@gmail.com>
2021-04-08 00:57:29 +0530

    While & do while loops in JS

--- intermediate/loops.js
+++ intermediate/loops.js
@@ -5,9 +5,27 @@
 
 const states = ['Raj','Guj',124,'Punjab','U.P','M.P']
 
+// For loop
 for (let i = 0; i < states.length; i++) {
     // if(typeof states[i] !== 'string') continue;
     // if(typeof states[i] !== 'number') continue;
     const element = states[i];
     console.log(element);
 }
+
+
+// while loop
+let i = 0;
+while(i < states.length) {
+    console.log(states[i]);
+    i++;
+}
+
+
+// do while loop
+let i = 0;
+do{
+console.log(i);
+    i++;
+}while(i<=20);
+// used when we need to execute the block of code at least once -- Hardware domains usage
