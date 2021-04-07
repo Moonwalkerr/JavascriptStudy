30323567271cf23cfc9c195fa76855617dafe464
Moonwalker <abhisk.mhra@gmail.com>
2021-04-08 00:34:55 +0530

    for loops in JS

---
+++ intermediate/loops.js
@@ -0,0 +1,13 @@
+// for (let i=0; i<10;i++){
+//     console.log(i);
+// }
+
+
+const states = ['Raj','Guj',124,'Punjab','U.P','M.P']
+
+for (let i = 0; i < states.length; i++) {
+    // if(typeof states[i] !== 'string') continue;
+    // if(typeof states[i] !== 'number') continue;
+    const element = states[i];
+    console.log(element);
+}
