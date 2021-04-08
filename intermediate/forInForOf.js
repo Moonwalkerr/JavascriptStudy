93d96f7b225673137688983bca23770532f71635
Moonwalker <abhisk.mhra@gmail.com>
2021-04-08 20:20:15 +0530

    For in and For of loops in JS

---
+++ intermediate/forInForOf.js
@@ -0,0 +1,23 @@
+
+// For of Loop : used mostly when iterate through arrays
+
+const names = ['Abhishek','Roy','Oliver','Bruce Wayne'];
+
+for (const n of names){
+    console.log(n);
+}
+
+
+
+// For in Loop : used mostly when iterate through objects
+
+const symbols = {
+    yt: "Youtube",
+    ig: "Instagram",
+    fb: "Facebook",
+    gh:"Github"
+};
+
+for(const symbol in symbols){
+    console.log(`This : ${symbol} is the symbol of ${symbols[symbol]}`);
+}
