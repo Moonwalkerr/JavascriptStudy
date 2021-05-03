6f5064d510d29d841f794ee6811b356874ed8d7e
Moonwalker <abhisk.mhra@gmail.com>
2021-05-03 21:03:13 +0530

    functional approach of declaring an obj

---
+++ moreInJS/functionalObj.js
@@ -0,0 +1,25 @@
+var User = function(firstName, lastName, courseCount) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.courseCount = courseCount;
+
+    this.getCourseCount = function() {
+        console.log(`Course Count = ${this.courseCount}`);
+    }
+}
+
+
+// regular function, this keyword points toward window obj, so undefined
+// var abhi = User("Moonwalker","Abhi",2);
+
+
+// new Keyword is responsible for:
+// using the default template,
+// invoking the constructor
+// instantiation
+var abhi = new User("Moonwalker","Abhi", 2);
+console.log(abhi); // user 1
+
+
+var user2 = new User("ABC","sda",5); // user 2
+console.log(user2.getCourseCount());
