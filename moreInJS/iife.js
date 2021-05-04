acfa7245f89f02607bc51f46999fb8c109cb561d
Moonwalker <abhisk.mhra@gmail.com>
2021-05-04 12:18:35 +0530

    Self Executing Anonymous function

---
+++ moreInJS/iife.js
@@ -0,0 +1,14 @@
+
+// Traditional function
+
+// function sayHello(){
+//     console.log("Hello");
+// }
+
+
+// Self Executing Anonymous function or Immediatedly invoked function expression
+
+(function(){
+        console.log("Hello");;
+    }
+)();
