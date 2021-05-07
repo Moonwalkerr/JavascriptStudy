5e506cceafd6679baf64d2a8a842cd944b910ab1
Moonwalker <abhisk.mhra@gmail.com>
2021-05-07 19:08:55 +0530

    Sorting Concept using JS inside the List HTML

--- DOM/TextNode&Elements/script.js
+++ DOM/TextNode&Elements/script.js
@@ -21,14 +21,12 @@ const courses = [
   },
 ];
 
-
-
-{/* <li>CourseName <span>Price</span> </li> */}
-
-
-
 const  generateLIST = () => {
   const ul = document.querySelector(".list-group");
+  
+  // The following is done to clear UL whever this function is called again during upcoming sorting event via Sort Button 
+  ul.innerHTML = "";
+
   courses.forEach((course) => {
 
     // Creating List Element
@@ -52,48 +50,13 @@ const  generateLIST = () => {
 
   })
 }
-
-
+// Adding Event listener on window object , whenever the page reloads, the list will get generated
 window.addEventListener("load",generateLIST);
 
 
-
-
-
-{
-  /* <li class="list-group-item">
-  Complete C++ course
-  <span class="float-right">$ 2.8</span>;
-</li>; */
-}
-
-// function generateLIST() {
-//   const ul = document.querySelector(".list-group");
-//   ul.innerHTML = "";
-//   courses.forEach((course) => {
-//     const li = document.createElement("li");
-//     li.classList.add("list-group-item");
-
-//     const name = document.createTextNode(course.name);
-//     li.appendChild(name);
-
-//     const span = document.createElement("span");
-//     span.classList.add("float-right");
-//     const price = document.createTextNode("$ " + course.price);
-//     span.appendChild(price);
-
-//     li.appendChild(span);
-//     ul.appendChild(li);
-//   });
-// }
-
-// // generateLIST();
-
-// window.addEventListener("load", generateLIST);
-
-// const button = document.querySelector(".sort-btn");
-
-// button.addEventListener("click", () => {
-//   courses.sort((a, b) => a.price - b.price);
-//   generateLIST();
-// });
+// Sorting Button
+const button = document.querySelector(".sort-btn");
+button.addEventListener("click",()=>{
+  courses.sort((a,b)=>a.price - b.price);
+  generateLIST();
+})
